134af266b79d49e4368805e2a99c652f33c33a3b
phineas6725 <30693448+Rahul6725@users.noreply.github.com>
2020-02-20 18:40:32 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,15 +1,21 @@
+function submitey() {
+    var dataString = document.getElementsByClassName("aboutyoufield");
+    var data = new Array();
 
+    for (var i = 0; i < dataString.length; i++) {
+        data[i] = dataString[i].value;
+        //document.getElementById("displayData").innerHTML += data[i];
+        // alert(data[i]);
 
-    function submitey() {
-        var dataString = document.getElementsByClassName("aboutyoufield");
-        var data = new Array();
-
-        for (var i = 0; i < dataString.length; i++) {
-            data[i] = dataString[i].value;
-            //document.getElementById("displayData").innerHTML += data[i];
-            // alert(data[i]);
-            
-        }
-        alert(data);
-        return data;
     }
+    alert(data);
+
+    if (demo(data)) {
+        alert("hello");
+    } else {
+        alert("error");
+    }
+
+    // return data;
+
+}
